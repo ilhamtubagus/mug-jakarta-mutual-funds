0adf8cbb24d22f365da7f133650bab08e133c332
Ilham.Arfian <ilham.arfian@btpn.com>
2023-08-13 19:21:07 +0700

    update routes path accounts

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,25 +1,23 @@
 const { Router } = require('express');
-const { authenticationMiddleware } = require('../middlewares');
 
 const router = Router();
 
-router.get('/test', authenticationMiddleware(), (req, res, next) => {
-  res.status(200).json(res.locals.auth);
-  return next();
-});
-
-router.post('/account', async (req, res, next) => {
+router.post('/accounts', async (req, res, next) => {
   const {
     accountService,
   } = res.locals;
 
-  const result = await accountService.register(req.body);
+  try {
+    const result = await accountService.register(req.body);
 
-  res.status(201).json(result);
-  return next();
+    res.status(201).json(result);
+    return next();
+  } catch (e) {
+    next(e);
+  }
 });
 
-router.post('/account/login', async (req, res, next) => {
+router.post('/accounts/login', async (req, res, next) => {
   const {
     accountService,
   } = res.locals;
