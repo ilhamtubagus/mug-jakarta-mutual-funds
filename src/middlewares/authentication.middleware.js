82c6f21b31e330babf1e121a4b1bca1c8c21cf2f
Ilham.Arfian <ilham.arfian@btpn.com>
2023-08-20 12:08:40 +0700

    fix logger error in authentication middleware

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -32,15 +32,15 @@ const _verifyToken = (token, audience, { secretKey, keyAlgorithm }) => {
 };
 
 const authenticate = (audience = CUSTOMER) => (req, res, next) => {
+  const { config: { jwt: jwtConfig }, logger } = req.app.locals;
   try {
-    const { config: { jwt: jwtConfig } } = req.app.locals;
     const token = _extractToken(req);
     const { userId, aud: role } = _verifyToken(token, audience, jwtConfig);
     const user = { userId, role };
     res.locals.auth = { user };
     return next();
   } catch (e) {
-    req.log.error(e);
+    logger.error(e);
     let error = e;
 
     if (error instanceof JsonWebTokenError || error instanceof TokenExpiredError) {
